3ddcd8754cda329bf375c6236d80e2d9454ce809
Sandro Lange <lange@softwareparadies.de>
2015-09-12 21:34:12 +0200

    - SD Card library testing

--- firmware/Sd2Card.cpp
+++ firmware/Sd2Card.cpp
@@ -21,6 +21,7 @@
 #include "application.h"
 #include "Sd2Card.h"
 
+#include "pinmap_hal.h"
 //#include "spark_wiring_spi.h"
 //#include "spark_wiring_usbserial.h"
 
@@ -762,10 +763,12 @@ uint8_t Sd2Card::sparkSPISend(uint8_t data) {
 	else {						// SPI Mode is Software so use bit bang method
 		for (uint8_t bit = 0; bit < 8; bit++)  {
 			if (data & (1 << (7-bit)))		// walks down mask from bit 7 to bit 0
+				pinSetFast
 				PIN_MAP[mosiPin_].gpio_peripheral->BSRRL = PIN_MAP[mosiPin_].gpio_pin; // Data High
 			else
 				PIN_MAP[mosiPin_].gpio_peripheral->BSRRH = PIN_MAP[mosiPin_].gpio_pin; // Data Low
 
+				pinResetFast
 			PIN_MAP[clockPin_].gpio_peripheral->BSRRL = PIN_MAP[clockPin_].gpio_pin; // Clock High
 
 			b <<= 1;
